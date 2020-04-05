70f63e0457e428637f906bf60e324f2546928ac9
daramedeiros <daraamedeiros@outlook.com>
2020-04-05 15:11:01 -0300

    inicio

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,16 +3,19 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
-
+let anos = [2020, 2021, 2022, 2023, 2024];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
 // ?
-
+function qualAno(args){
+    return args;
+}
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
 // ?
+qualAno(args)[2];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
